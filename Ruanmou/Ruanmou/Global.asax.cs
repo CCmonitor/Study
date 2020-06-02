841d969a18967e81c9eda48c969a9e741efb46eb
CCmonitor <caientao@foxmail.com>
2020-06-02 11:02:04 +0800

    submit

--- Ruanmou/Ruanmou/Global.asax.cs
+++ Ruanmou/Ruanmou/Global.asax.cs
@@ -12,7 +12,7 @@ namespace Ruanmou
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();//注册区域
+            AreaRegistration.RegisterAllAreas();//注册区域11111
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);//注册全局的Filter
             RouteConfig.RegisterRoutes(RouteTable.Routes);//注册路由
             BundleConfig.RegisterBundles(BundleTable.Bundles);

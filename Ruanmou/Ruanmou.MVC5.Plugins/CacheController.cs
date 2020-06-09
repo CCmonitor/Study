2017cbdacc76177c7881e0e16ec4085dba2c7236
CCmonitor <caientao@foxmail.com>
2020-06-09 13:54:40 +0800

    submit

---
+++ Ruanmou/Ruanmou.MVC5.Plugins/CacheController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Ruanmou.MVC5.Plugins
+{
+    public class CacheController : Controller
+    {
+        public ActionResult CacheIndex()
+        {
+
+            return View("~/views/home/index.cshtml");
+        }
+    }
+}

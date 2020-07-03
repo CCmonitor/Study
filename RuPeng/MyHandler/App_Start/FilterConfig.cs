00b2451aa3f80b29a690eb9d81758d3aa6f2415f
CCmonitor <caientao@foxmail.com>
2020-07-03 16:44:50 +0800

    submit

---
+++ RuPeng/MyHandler/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MyHandler
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ RuPeng/MyHandler/MyFirstHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.SessionState;
+
+namespace MyHandler
+{
+    /// <summary>
+    /// 目的：实现一个简单的自定义HttpHandler容器
+    /// </summary>
+    public class MyFirstHandler : IHttpHandler, IRequiresSessionState
+    {
+        #region IHttpHandler 成员
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("<h1><b>Hello HttpHandler</b></h1>");
+            context.Session["Test"] = "测试HttpHandler容器中调用Session";
+            context.Response.Write(context.Session["Test"]);
+        }
+
+        #endregion
+    }
+}

f92ce4299d17068a7f4b9e89f4cb22d6db6913e3
CCmonitor <caientao@foxmail.com>
2020-07-10 10:28:04 +0800

    submit

--- RuPeng/@MVC_01/Controllers/DefaultController.cs
+++ RuPeng/@MVC_01/Controllers/DefaultController.cs
@@ -51,5 +51,30 @@ namespace _MVC_01.Controllers
             string age = fc["age"];
             return Content("name:" + name + "age:" + age);
         }
+        /// <summary>
+        /// Action的方法不能重载，
+        /// 所以一个Controller中不能同时存在这二个Action:
+        /// public ActionResultT1(string name);
+        /// public ActionResult T1(int age);
+        /// 特殊情况：给Action方法上标注[HttpGet],[HttpPost]
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult F3()
+        {
+            return Content("1111");
+        }
+        /// <summary>
+        /// 特殊情况：给Action方法上标注[HttpGet],[HttpPost]，
+        /// 这样当发出Get请求的时候执行标注[HttpGet]的Action方法，
+        /// 当发出post请求的时候执行标注[HttpPost]的Action方法。没有标注的，
+        /// 常见的应用：标注[HttpGet]的Action展示初始页面，
+        /// 标注[HttpPost]的Action处理表单提交,案例：做一个报名页面
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public ActionResult F3(string name)
+        {
+            return Content("名字" + name);
+        }
     }
 }
